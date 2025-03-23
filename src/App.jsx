03964308ab45aa0ae5085786b5401b8afefb9b1b
thelma-github <thelmabuabeng4@gmail.com>
2025-03-23 20:09:26 +0000

    Update App.jsx
    import { BrowserRouter, Routes, Route } from "react-router-dom";
    
    // Components
    import Header from "./components/Header.jsx";
    import Footer from "./components/Footer.jsx";
    import ErrorBoundary from "./components/ErrorBoundary.jsx"; // Import ErrorBoundary
    
    // Pages
    import Home from "./pages/Home.jsx";
    import Categories from "./pages/Categories.jsx";
    import Recipes from "./pages/Recipes.jsx";
    import Blogs from "./pages/Blogs.jsx";
    import AboutUs from "./pages/AboutUs.jsx";
    import ContactUs from "./pages/ContactUs.jsx";
    import Pages from "./pages/Pages.jsx";
    import Login from "./pages/Login.jsx";
    import Signup from "./pages/Signup.jsx";
    import NotFound from "./pages/NotFound.jsx";
    
    import CategoryCard from "./components/CategoryCard.jsx";
    
    
    function App() {
      return (
        <BrowserRouter>
          <ErrorBoundary>
            <Header />
          </ErrorBoundary>
    
          <Routes>
            <Route path="/" element={<Home />} />
            <Route path="/categories" element={<Categories />} />
            <Route path="/recipes" element={<Recipes />} />
            <Route path="/blogs" element={<Blogs />} />
            <Route path="/about" element={<AboutUs />} />
            <Route path="/contact" element={<ContactUs />} />
            <Route path="/pages" element={<Pages />} />
            <Route path="/login" element={<Login />} />
            <Route path="/signup" element={<Signup />} />
            <Route path="*" element={<NotFound />} />
          </Routes>
    
          <ErrorBoundary>
            <Footer />
          </ErrorBoundary>
        </BrowserRouter>
      );
    }
    
    export default App;

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,51 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Components
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx"; // Import ErrorBoundary
 
 // Pages
-import Home from "./pages/Home";
-import Categories from "./pages/Categories";
-import RecipeDetails from "./pages/RecipeDetails";
-import Blogs from "./pages/Blogs";
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
-import Pages from "./pages/Pages";
-import Login from "./pages/Login";     // 👈 Import Login
-import Signup from "./pages/Signup";   // 👈 Import Signup
-import NotFound from "./pages/NotFound";
+import Home from "./pages/Home.jsx";
+import Categories from "./pages/Categories.jsx";
+import Recipes from "./pages/Recipes.jsx";
+import Blogs from "./pages/Blogs.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
+import ContactUs from "./pages/ContactUs.jsx";
+import Pages from "./pages/Pages.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+import CategoryCard from "./components/CategoryCard.jsx";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <ErrorBoundary>
+        <Header />
+      </ErrorBoundary>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/recipes" element={<Recipes />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/pages" element={<Pages />} />
-        <Route path="/login" element={<Login />} />       {/* ✅ Login route */}
-        <Route path="/signup" element={<Signup />} />     {/* ✅ Signup route */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
+
+      <ErrorBoundary>
+        <Footer />
+      </ErrorBoundary>
     </BrowserRouter>
   );
 }
 
 export default App;
+
